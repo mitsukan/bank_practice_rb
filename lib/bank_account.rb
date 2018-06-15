1ad117cd7f721c2e20f288872527c0a4a6b0c7ca
Jihin Ip <jim_ip20@hotmail.com>
2018-06-15 13:08:17 +0100

    Passing tests for methods to have amount and date arguments

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,18 @@
 class Bank_Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(amount)
-    @balance += amount 
+  def deposit(amount, date)
+    @balance += amount
+  end
+
+  def withdraw(amount, date)
+    @balance -= amount
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,11 +6,29 @@ describe Bank_Account do
     expect(subject.balance).to eq(0)
   end
 
+  it "can store transactions" do
+    expect(subject.transactions).to eq([])
+  end
+
   describe ".deposit" do
 
-    it "adds money to the bank account" do
-      subject.deposit(10)
+    it "adds money to the bank account at a specified date" do
+      subject.deposit(10, "12/12/2012")
       expect(subject.balance).to eq(10)
+    end
+
+    it "adds the deposit as a transaction" do
+      subject.deposit(10, "12/12/2012")
+      expect(subject.transactions).to eq(["12/12/2012", 10])
+    end
+
+  end
+
+  describe ".withdraw" do
+
+    it "subtracts money from the bank account" do
+      subject.withdraw(10, "12/12/2012")
+      expect(subject.balance).to eq(-10)
     end
 
   end
